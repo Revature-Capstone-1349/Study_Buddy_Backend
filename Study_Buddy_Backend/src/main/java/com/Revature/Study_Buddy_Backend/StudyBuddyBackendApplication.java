69245c84c4b4b442a845f8a814ff1c1dc4df91b5
ttlin030735 <thomaslin030735@gmail.com>
2022-06-21 18:13:23 -0400

    Startup

---
+++ Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/StudyBuddyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Revature.Study_Buddy_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBuddyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyBuddyBackendApplication.class, args);
+	}
+
+}
---
+++ Study_Buddy_Backend/src/test/java/com/Revature/Study_Buddy_Backend/StudyBuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Revature.Study_Buddy_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBuddyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
