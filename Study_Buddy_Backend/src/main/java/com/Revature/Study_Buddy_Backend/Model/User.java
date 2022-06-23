976908b737be989e0f58fc2b5e3ae4b93e7ff81e
Manisha Goyal <19manishagoyal76@gmail.com>
2022-06-23 15:08:25 -0500

    user

--- Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Model/User.java
+++ Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Model/User.java
@@ -1,5 +1,24 @@
 package com.Revature.Study_Buddy_Backend.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long userid;
+    private String email;
+    private String passwd;
+    private String name;
     
 }
--- Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Repository/UserRepo.java
+++ Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Repository/UserRepo.java
@@ -1,4 +1,9 @@
 package com.Revature.Study_Buddy_Backend.Repository;
 
-public interface UserRepo {
+import com.Revature.Study_Buddy_Backend.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Long> {
 }
