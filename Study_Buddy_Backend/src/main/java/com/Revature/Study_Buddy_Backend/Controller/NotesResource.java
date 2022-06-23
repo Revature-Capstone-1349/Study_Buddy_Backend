7ea8a2480387f3056764e981e1d8f543e223ba44
Dovlet <dovletx@gmail.com>
2022-06-23 17:06:35 -0400

    pulling

---
+++ Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Controller/NotesResource.java
@@ -0,0 +1,8 @@
+package com.Revature.Study_Buddy_Backend.Controller;
+
+
+public class NotesResource {
+
+    private int id;
+
+}
---
+++ Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Model/Notes.java
@@ -0,0 +1,2 @@
+package com.Revature.Study_Buddy_Backend.Model;public class Notes {
+}
