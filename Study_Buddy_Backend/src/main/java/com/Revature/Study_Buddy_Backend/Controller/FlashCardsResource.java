efaede9ecc5b9600a99c23c333c27f1629efbfed
ttlin030735 <thomaslin030735@gmail.com>
2022-06-23 18:08:11 -0400

    flashcards fixes

--- Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Controller/FlashCardsResource.java
+++ Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Controller/FlashCardsResource.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/flashcards")
 public class FlashCardsResource {
-
+    @Autowired
     private FlashCardsService flashCardsService;
     @GetMapping
     public ResponseEntity<List<FlashCards>>getAllFlashCards() {
