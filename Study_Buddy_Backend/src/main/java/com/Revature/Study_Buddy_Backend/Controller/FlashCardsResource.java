84ace2f5dac278af48c122e92f662830e7eb454c
Khine Zaw <khinekzaw7@gmail.com>
2022-06-25 20:52:59 -0400

    phrase 2 new changes

--- Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Controller/FlashCardsResource.java
+++ Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Controller/FlashCardsResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
+
 import java.util.List;
 
 @AllArgsConstructor
@@ -16,64 +17,69 @@ public class FlashCardsResource {
     @Autowired
     private FlashCardsService flashCardsService;
 
+    /*1.  Get FlashCardsList */
     @GetMapping
-    public ResponseEntity<List<FlashCards>> getAllFlashCards() {
-        return new ResponseEntity<>(flashCardsService.getAllFlashCards(),HttpStatus.OK);
+    public ResponseEntity<List<FlashCards>> FlashCardsList() {
+        return new ResponseEntity<>(flashCardsService.getAllFlashCardsList(), HttpStatus.OK);
     }
 
-    /*
-    try catch null return/ no content
+    /*2.
+        getMapping
+
+        try catch if null or size is 0
+        implement findBysetId
+
+        call getAllFlashCards return list base off setId
+     */
+    @GetMapping("/getSet/{fCardId}")
+    public ResponseEntity<List<FlashCards>> getAllFlashCardsBySetId(@PathVariable("fCardId") Long id) {
+        return new ResponseEntity<>(flashCardsService.getBysetId(id), HttpStatus.OK);
+
+    }
+
+    /*3.
+        try catch null return/ no content
      */
     @GetMapping("/{fCardId}")
     public ResponseEntity<FlashCards> getFlashCardsById(@PathVariable("fCardId") Long id) {
-        try{
+        try {
             return new ResponseEntity<>(flashCardsService.getByfCardId(id), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(flashCardsService.getByfCardId(id), HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
     }
 
+    /*4.  Get FlashCardsList */
     @PostMapping
-    public ResponseEntity<FlashCards> addFlashCards (@RequestBody FlashCards flashCards) {
+    public ResponseEntity<FlashCards> addFlashCards(@RequestBody FlashCards flashCards) {
         return new ResponseEntity<>(flashCardsService.addFlashCards(flashCards), HttpStatus.OK);
     }
 
-    /*
+    /*5.
     try catch null return/ no content
      */
     @PutMapping
-    public ResponseEntity<FlashCards> updateFlashCards (@RequestBody FlashCards flashCards) {
+    public ResponseEntity<FlashCards> updateFlashCards(@RequestBody FlashCards flashCards) {
         try {
+            FlashCards findCard = flashCardsService.getByfCardId(flashCards.getFCardId());
             return new ResponseEntity<>(flashCardsService.updateFlashCards(flashCards), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(flashCardsService.updateFlashCards(flashCards), HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
     }
-    /*
+
+    /*6.
     try catch null return/ no content
      */
     @DeleteMapping("/{fCardId}")
-    public ResponseEntity<?> deleteByFCardId (@PathVariable ("fCardId")Long fCardId){
-            try{
-                flashCardsService.getByfCardId(fCardId);
-                return new ResponseEntity<>(HttpStatus.OK);
-            } catch (Exception e) {
+    public ResponseEntity<?> deleteByFCardId(@PathVariable("fCardId") Long fCardId) {
+        try {
+            flashCardsService.getByfCardId(fCardId);
+            flashCardsService.deleteByfCardId(fCardId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
-         flashCardsService.deleteByfCardId(fCardId);
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-        /*
-    getMapping
-
-    try catch if null or size is 0
-    implement findBysetId
-
-    call getAllFlashCards return list base off setId
-     */
-    @GetMapping("{fCardId}")
-    public ResponseEntity<List<FlashCards>> getAllFlashCardsBysetId(@PathVariable("fCardId") Long id) {
-            return new ResponseEntity<>(flashCardsService.getFlashsBySetId(id), HttpStatus.OK);
-
-    }
 }
--- Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Service/FlashCardsService.java
+++ Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Service/FlashCardsService.java
@@ -2,64 +2,49 @@ package com.Revature.Study_Buddy_Backend.Service;
 
 import com.Revature.Study_Buddy_Backend.Model.FlashCards;
 import com.Revature.Study_Buddy_Backend.Repository.FlashCardsRepo;
+import com.Revature.Study_Buddy_Backend.Repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
-public class FlashCardsService{
+public class FlashCardsService {
     @Autowired
     private FlashCardsRepo flashCardsRepo;
-    public List<FlashCards> getAllFlashCards(){
+
+    /*1. try catch if exist*/
+    public List<FlashCards> getAllFlashCardsList() {
         return flashCardsRepo.findAll();
     }
 
-    // try catch if exist
-    public FlashCards getByfCardId (Long id) {
-        try {
-            return flashCardsRepo.findByfCardId(id);
-        } catch (Exception e) {
-            return flashCardsRepo.findByfCardId(id);
-        }
-    }
-    public FlashCards addFlashCards (FlashCards flashCards){
-        return flashCardsRepo.save(flashCards);
-    }
-
-    //try catch find flash card if not exist return null
-    public FlashCards updateFlashCards (FlashCards flashCards){
-            try{
-                return flashCardsRepo.save(flashCards);
-            } catch (Exception e){
-                return flashCardsRepo.save(flashCards);
-        }
-
-    }
-
-    //try catch find flash card if not exist return null
-    public void deleteByfCardId (Long fCardId) {
-        try {
-            flashCardsRepo.deleteByfCardId(fCardId);
-        } catch (Exception e) {
-            flashCardsRepo.deleteByfCardId(fCardId);
-        }
-    }
-
-    /*
+    /*2.
     try catch if null or size is 0
     implement findBysetId
 
-    call getAllFlashCards return list base off setId
+    call getAllFlashCardsList return list base off setId
      */
+    public List<FlashCards> getBysetId(Long setId) {
+        return flashCardsRepo.findBySetId(setId);
+    }
 
+    /*3. try catch if exist*/
+    public FlashCards getByfCardId(Long fCardId) {
+        return flashCardsRepo.findById(fCardId).get();
+    }
 
-   public List<FlashCards> getFlashsBySetId(Long setId){
-       try {
-           return flashCardsRepo.findBySetId(setId);
-       }
-       catch (Exception e){
-           return null;
-       }
-   }
+    /*4. adding flashcards*/
+    public FlashCards addFlashCards(FlashCards flashCards) {
+        return flashCardsRepo.save(flashCards);
+    }
 
+    /*5. try catch find flash card if not exist return null*/
+    public FlashCards updateFlashCards(FlashCards flashCards) {
+        return flashCardsRepo.save(flashCards);
+    }
+
+    /*6. try catch find flash card if not exist return null*/
+    public void deleteByfCardId(Long fCardId) {
+        flashCardsRepo.deleteById(fCardId);
+    }
 }
