38befbad0a085b1904a711d8e32b0a1067d98f62
Connor Bullock <cbullock1@ufl.edu>
2022-06-27 20:51:14 -0400

    Modified the note search by setById

--- Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Repository/NotesRepo.java
+++ Study_Buddy_Backend/src/main/java/com/Revature/Study_Buddy_Backend/Repository/NotesRepo.java
@@ -11,5 +11,6 @@ public interface NotesRepo extends JpaRepository <Notes, Long> {
     List<Notes> findByUserId(Long setId);
     List<Notes> findBySetId(Long setId);
     Notes findBynotesId(Long notesId);
+    List<Notes> findBySetIdAndUserId(Long setId, Long userId);
 
 }
